f3efe4960e8932dc3d8bdeb933ea75280d3d056b
Fockus <fockus@gmail.com>
2014-12-14 05:06:57 +0300

    Add UI-kit

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -4,3 +4,23 @@ RSpec.describe Question, :type => :model do
   it { should validate_presence_of :title }
   it { should validate_presence_of :body }
 end
+
+
+require 'rails_helper'
+
+RSpec.describe Question, :type => :model do
+  it 'has a valid factory' do
+    expect(build(:question)).to be_valid
+  end
+
+  it 'is invalid without title' do
+    expect(build(:question, title: nil)).not_to be_valid
+  end
+
+  it 'is invalid without body' do
+    expect(build(:question, body: nil)).not_to be_valid
+  end
+  #it { should validates_presence_of :title }
+  #it { should validates_presence_of :body }
+end
+
