7366f5beaedfac2fa2af9847799ceff4b6cbb903
Fockus <fockus@gmail.com>
2015-03-24 09:28:42 +0300

    change macros

--- spec/support/acceptance_macros.rb
+++ spec/support/acceptance_macros.rb
@@ -2,15 +2,15 @@ module AcceptanceMacros
   def sign_in(user)
     visit new_user_session_path
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    click_on 'Sing in'
+    fill_in 'Password', with: user.password, :match => :prefer_exact
+    click_button 'Sign in'
   end
 
   def sign_up(user)
     visit new_user_registration_path
     fill_in 'Email', with: user.email
-    fill_in 'Password', with: user.password
-    fill_in 'Password confirmation', with: user.password_confirmation
-    click_on 'Sign up'
+    fill_in 'Password', with: user.password, :match => :prefer_exact
+    fill_in 'Password confirmation', with: user.password_confirmation, :match => :prefer_exact
+    click_button 'Sign up'
   end
 end
